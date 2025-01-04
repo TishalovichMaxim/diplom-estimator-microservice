92bc6f5e1175d3de777cfd7ceba8e255225df305
Tishalovich Maxim <tishalovichm@gmail.com>
2025-01-04 21:39:14 +0300

    Init

---
+++ src/main/java/by/tishalovichm/solutionestimator/SolutionEstimatorApplication.java
@@ -0,0 +1,13 @@
+package by.tishalovichm.solutionestimator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolutionEstimatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolutionEstimatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/tishalovichm/solutionestimator/SolutionEstimatorApplicationTests.java
@@ -0,0 +1,13 @@
+package by.tishalovichm.solutionestimator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolutionEstimatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
